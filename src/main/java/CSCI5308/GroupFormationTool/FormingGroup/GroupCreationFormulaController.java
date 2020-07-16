db9ceb48cb59058b8d9fbaaa33b3d1b33590f9c9
Hardik <hardikdudhrejia.0719@gmail.com>
2020-07-15 22:53:06 -0300

    Added view to display the created groups

--- src/main/java/CSCI5308/GroupFormationTool/FormingGroup/GroupCreationFormulaController.java
+++ src/main/java/CSCI5308/GroupFormationTool/FormingGroup/GroupCreationFormulaController.java
@@ -1,5 +1,7 @@
 package CSCI5308.GroupFormationTool.FormingGroup;
 
+import CSCI5308.GroupFormationTool.AccessControl.User;
+import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -9,13 +11,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 @RequestMapping("/groupformation")
 public class GroupCreationFormulaController 
 {
 	private Logger log = LoggerFactory.getLogger(GroupCreationFormulaController.class);
 	private static final String GROUP_FORMATION_VIEW = "groupformation";
-	
+
 	@GetMapping
 	public String groupFormation()
 	{
@@ -32,7 +37,6 @@ public class GroupCreationFormulaController
 			@RequestParam String studentInclude,
 			@RequestParam String freetext)
 	{
-		
 		log.info("Parameteres passed are: groupSize " + groupSize + " mchoiceChoose1: " + mchoice_choose1 
 				+ " mchoiceChooseMany: " + mchoice_chooseMany + " numeric: " + numeric + " xValue: " + xValue
 				+ " studentInclude: " + studentInclude + " freeText: " + freetext );
@@ -42,9 +46,6 @@ public class GroupCreationFormulaController
 			model.addAttribute("error", "Group Size cannot be less than 2");
 			return GROUP_FORMATION_VIEW;
 		}
-		
 		return GROUP_FORMATION_VIEW;
 	}
-	
-
 }
